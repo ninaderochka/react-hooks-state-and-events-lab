2055d86156648d57e65da7f662501997cb49125a
Nina Hawari <nina.hawari@gmail.com>
2023-01-05 23:58:44 +0200

    progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import { useState } from "react";
 
 function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [ dark, setDark ] = useState('')
+  const [ light, setLight ] = useState('')
+  const appClass = {dark} ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
+      <div className="App dark">
+{setDark}
+</div>
+
+<div className="App light">
+{setLight}
+</div>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={setDark}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+const [ item, addItem ] = useState('')
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addItem}>Add to Cart</button>
     </li>
   );
 }
